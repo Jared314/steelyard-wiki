6ec3dd38b825a8e2ab269eddd4dede9206dc8701
Jared Lobberecht <pretender314@gmail.com>
2013-04-29 20:53:54 -0500

    first sqlite db query success

--- clojure/src/steelyard_wiki/server.clj
+++ clojure/src/steelyard_wiki/server.clj
@@ -3,15 +3,27 @@
             [compojure.core :refer :all]
             [compojure.route :as route]
             [compojure.handler :as handler]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.java.jdbc :as j]
+            [clojure.java.jdbc.sql :as s]))
 
-(def default-connection-string "")
+(def default-connection-string "Data Source=db.sqlite;Version=3;")
+
+(def dbconn
+      {:classname "org.sqlite.JDBC"
+       :subprotocol "sqlite"
+       :subname "db.sqlite"
+       :version "3"})
+
+(defn get-data [name]
+      (last (j/query dbconn (s/select * :page (s/where {:name name}))
+                     :row-fn :value)))
 
 (defn parse-uri [uri]
       (str/replace uri #"^\/" ""))
 
 (defroutes site-routes
-  (GET "/*" {uri :uri} (str "<h1>" (parse-uri uri) "</h1>"))
+  (GET "/*" {uri :uri} (get-data (parse-uri uri)))
   (route/resources "/")
   (route/not-found (str "not found")))
 
